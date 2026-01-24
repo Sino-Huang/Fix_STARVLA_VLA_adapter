1fb94dd5b4a32bc543a83cd4593e75a264ee779b
Sukai Huang <sukai.huang@monash.edu>
2026-01-24 17:44:43 +1100

    slurm test update

--- slurm_script/calculate_cnn_mlp_vlm_u.sh
+++ slurm_script/calculate_cnn_mlp_vlm_u.sh
@@ -15,10 +15,10 @@
 CONDA_BASE="$HOME/miniconda3"
 source "$CONDA_BASE/etc/profile.d/conda.sh"
 
-source ~/cd_vla.sh
+cur_dir=$(pwd)
 
-# 
+source ~/cd_starvla
 
-cd ~
+echo "Python location is at $(which python)"
 
 python ${cur_dir}/slurm_script/cnn_mlp_vlm_switch_logic.py
