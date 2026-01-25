f4c1bbe112b7b96a2a4461fce015029c5f0a85da
Sukai Huang <sukai.huang@monash.edu>
2026-01-25 18:29:21 +1100

    minor

---
+++ scripts/connect_utils/sync_from_c1_to_c3.sh
@@ -0,0 +1,5 @@
+rsync -chavP /home/sukai/Project/STAR_VLA_FIX/Fix_STARVLA_VLA_adapter sukai@darpa_5090_c2:/home/sukai/Project/STAR_VLA_FIX/ --delete
+
+rsync -chavP /home/sukai/cd_starvla sukai@darpa_5090_c2:/home/sukai/
+
+rsync -chavP /home/sukai/cd_libero sukai@darpa_5090_c2:/home/sukai/
