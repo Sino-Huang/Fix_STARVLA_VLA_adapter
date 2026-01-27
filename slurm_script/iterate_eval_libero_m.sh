70eeebb9fa6701e23870392bdcff3094845ac4e3
Sukai Huang <sukai.huang@monash.edu>
2026-01-27 12:33:12 +1100

    minor fix to support monitor different node

--- slurm_script/iterate_eval_libero_m.sh
+++ slurm_script/iterate_eval_libero_m.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 echo "Best run this in tmux"
+node_name=$1
 # run squeue | grep sukai and see if both node12 and PD (Resources) status exist
 
 while true; do
-    node12_count=$(squeue | grep sukaih | grep -c node12)
+    node_count=$(squeue | grep sukaih | grep -c "${node_name}")
     pd_count=$(squeue | grep sukaih | grep -c "PD")
     
-    if [ "$node12_count" -ge 1 ] && [ "$pd_count" -ge 1 ]; then
-        echo "$(date): node12 and PD (Resources) both present"
+    if [ "$node_count" -ge 1 ] && [ "$pd_count" -ge 1 ]; then
+        echo "$(date): ${node_name} and PD (Resources) both present"
         sleep 5
     else
-        echo "$(date): node12 or PD not found, submitting job..."
+        echo "$(date): ${node_name} or PD not found, submitting job..."
         sbatch scripts/eval_script/eval_libero_goal_tmux.sh
         sleep 5
     fi
