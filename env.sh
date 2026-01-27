5e8e7fb83254bc96e0f6cf8ba114b2a5acf070eb
Sukai Huang <sukai.huang@monash.edu>
2026-01-27 12:29:34 +1100

    minor

--- env.sh
+++ env.sh
@@ -15,11 +15,11 @@ else
     unset NCCL_SOCKET_IFNAME
 fi
 
-if [[ "$device_name" == *"fitcluster"* ]]; then
+if [[ "$device_name" == *"fitcluster"* ]] || [[ "$device_name" == *"spartan"* ]]; then
     export CUDA_VISIBLE_DEVICES=0,1 # only 2 GPUs on fit cluster
 elif [[ "$device_name" == *"node"* ]]; then
     export CUDA_VISIBLE_DEVICES=0,1 # all 4 GPUs on unimelb node12
-elif [[ "$device_name" == *"darpa"* ]] || [[ "$device_name" == *"ansr-5090"* ]] || [[ "$device_name" == *"spartan"* ]]; then
+elif [[ "$device_name" == *"darpa"* ]] || [[ "$device_name" == *"ansr-5090"* ]]; then
     export CUDA_VISIBLE_DEVICES=0,1,2,3 # all 4 GPUs on darpa server
 else
     echo "This script is only for fitcluster or darpa device. Current device: ${device_name}"
